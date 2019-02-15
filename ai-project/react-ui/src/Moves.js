7fed3184b3b6f8737ad822ed76fde856a068ae7c
Raj Patel <jpapadok@gmail.com>
2019-02-15 00:04:44 -0500

    created coloring for moves

--- ai-project/react-ui/src/Moves.js
+++ ai-project/react-ui/src/Moves.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import classNames from 'classnames';
 import purple from '@material-ui/core/colors/purple';
@@ -18,6 +18,47 @@ const styles = theme => ({
   }
 });
 
+function getColors(typeColors){
+  console.log(typeColors);
+  var themes = {}
+  for(var i in Object.keys(typeColors)){
+    var color = Object.keys(typeColors)[i];
+    themes[color] = createMuiTheme({
+      palette: {
+        primary: {
+          main: typeColors[color]
+        },
+      },
+      typography: {
+        useNextVariants: true,
+      },
+    });
+  }
+  return themes
+}
+
+const typeColors = {
+  "bug": "#A8B820",
+	"dark": "#705848",
+	"dragon": "#7038F8",
+	"electric": "#F8D030",
+	"fairy": "#EE99AC",
+	"fighting": "#C03028",
+	"fire": "#F08030",
+	"flying": "#A890F0",
+	"ghost": "#705898",
+	"grass": "#78C850",
+	"ground": "#E0C068",
+	"ice": "#98D8D8",
+	"normal": "#A8A881",
+	"poison": "#A040A0",
+	"psychic": "#F85888",
+	"rock": "#B8A038",
+	"steel": "#B8B8D0",
+	"water": "#6890F0",
+}
+
+const theme =  getColors(typeColors);
 
 class Moves extends React.Component {
 
@@ -49,38 +90,46 @@ class Moves extends React.Component {
     var names = this.getMoveNames(this.props.moves)
     return (
       <div>
-        <Button
-          variant="contained"
-          color="primary"
-          className={classNames(classes.margin, classes.cssRoot)}
-          onClick={() => { this.handleInput(">p1 move 1", null); }}
-        >
-          {names[0]}
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          className={classNames(classes.margin, classes.cssRoot)}
-          onClick={() => { this.handleInput(">p1 move 2", null); }}
-        >
-          {names[1]}
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          className={classNames(classes.margin, classes.cssRoot)}
-          onClick={() => { this.handleInput(">p1 move 3", null); }}
-        >
-          {names[2]}
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          className={classNames(classes.margin, classes.cssRoot)}
-          onClick={() => { this.handleInput(">p1 move 4", null); }}
-        >
-          {names[3]}
-        </Button>
+        <MuiThemeProvider theme={theme.fire}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.margin}
+            onClick={() => { this.handleInput(">p1 move 1", null); }}
+          >
+            {names[0]}
+          </Button>
+        </MuiThemeProvider>
+        <MuiThemeProvider theme={theme.fire}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.margin}
+            onClick={() => { this.handleInput(">p1 move 2", null); }}
+          >
+            {names[1]}
+          </Button>
+        </MuiThemeProvider>
+        <MuiThemeProvider theme={theme.fire}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.margin}
+            onClick={() => { this.handleInput(">p1 move 3", null); }}
+          >
+            {names[2]}
+          </Button>
+        </MuiThemeProvider>
+        <MuiThemeProvider theme={theme.fire}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.margin}
+            onClick={() => { this.handleInput(">p1 move 4", null); }}
+          >
+            {names[3]}
+          </Button>
+        </MuiThemeProvider>
       </div>
     );
   }
