d11e6c6362dcf1175d11d2e7d761d8fcf80b3ba6
Raj Patel <jpapadok@gmail.com>
2019-06-07 22:20:04 -0400

    added tab view

--- mobile/showdown-app/App.js
+++ mobile/showdown-app/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
 
 export default class App extends React.Component {
+  state = { selectedTab: 'home' };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <TabNavigator>
+        <TabNavigator.Item
+          selected={this.state.selectedTab === 'home'}
+          title="Home"
+          renderIcon={() => <Image source={require('./assets/home.png')} />}
+          renderSelectedIcon={() => <Image source={require('./assets/home.png')} />}
+          badgeText="!"
+          onPress={() => this.setState({ selectedTab: 'home' })}>
+          <View />
+        </TabNavigator.Item>
+        <TabNavigator.Item
+          selected={this.state.selectedTab === 'profile'}
+          title="Profile"
+          renderIcon={() => <Image source={require('./assets/home.png')} />}
+          renderSelectedIcon={() => <Image source={require('./assets/home.png')} />}
+          renderBadge={() => <View />}
+          onPress={() => this.setState({ selectedTab: 'profile' })}>
+          <View />
+        </TabNavigator.Item>
+        <TabNavigator.Item
+          selected={this.state.selectedTab === 'profile'}
+          title="Profile"
+          renderIcon={() => <Image source={require('./assets/home.png')} />}
+          renderSelectedIcon={() => <Image source={require('./assets/home.png')} />}
+          renderBadge={() => <View />}
+          onPress={() => this.setState({ selectedTab: 'profile' })}>
+          <View />
+        </TabNavigator.Item>
+      </TabNavigator>
     );
   }
 }
@@ -16,6 +45,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
